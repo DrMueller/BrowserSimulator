12b0ebb0e19757e9eb55fcbe824b4a36cd48125e
Matthias Müller <matthias.mueller@trivadis.com>
2020-01-09 21:13:48 +0100

    Init

---
+++ BrowserWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BrowserSimulator
+{
+    /// <summary>
+    /// Interaction logic for BrowserWindow.xaml
+    /// </summary>
+    public partial class BrowserWindow : Window
+    {
+        public BrowserWindow()
+        {
+            InitializeComponent();
+            HideScriptErrors(WebBrowser);
+        }
+
+        private void HideScriptErrors(WebBrowser webBrowser)
+        {
+            var webBrowserField = typeof(WebBrowser).GetField("_axIWebBrowser2", BindingFlags.Instance | BindingFlags.NonPublic);
+            if (webBrowserField == null)
+            {
+                return;
+            }
+
+            var objComWebBrowser = webBrowserField.GetValue(webBrowser);
+            if (objComWebBrowser == null)
+            {
+                WebBrowser.Loaded += (o, s) => HideScriptErrors(webBrowser); //// In case we are to early
+                return;
+            }
+
+            objComWebBrowser.GetType().InvokeMember("Silent", BindingFlags.SetProperty, null, objComWebBrowser, new object[] { true });
+        }
+    }
+}
---
+++ Models/Elements/WebElement.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BrowserSimulator.Models.Elements
+{
+    class WebElement
+    {
+    }
+}
